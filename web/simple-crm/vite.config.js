9ea5bd754ea3f7cf91d1c32c4c78115ecc730706
Luke Huang <2479853235@qq.com>
2025-09-11 11:26:13 +0800

    Change Vite server host to '0.0.0.0'

--- web/simple-crm/vite.config.js
+++ web/simple-crm/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   base: './', // 使用相对路径，解决静态托管部署时的资源加载问题
   // Vite 会自动加载 .env 文件中以 VITE_ 开头的环境变量
   server: {
-    host: '127.0.0.1',  // 使用IP地址代替localhost
+    host: '0.0.0.0',  // 使用IP地址代替localhost
     proxy: {
       '/__auth': {
         target: 'https://tcb-advanced-a656fc-1257967285.tcloudbaseapp.com/',
